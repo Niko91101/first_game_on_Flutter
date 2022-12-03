5fa83ceac3af13e6845e1dd3733a2f404880c1fd
Niko91101 <muz-stanislav@yandex.ru>
2022-12-03 19:37:29 +0300

    main game initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:lesson_1/gameCore/main_loop.dart';
 
 void main() {
-  startLoop();
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]).whenComplete(() {
+      SystemChrome.setEnabledSystemUIOverlays([
+        SystemUiOverlay.bottom
+      ]);
 
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Center(
+      child: Text("Game"),
+    ),
+  ));
+    
+  });
 }
